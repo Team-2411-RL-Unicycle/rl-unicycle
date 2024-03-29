799168099ae1a1585fe4ccdba5f774bf3751ad27
Tristan Lee <tristan.rene.lee@gmail.com>
2024-03-28 17:29:56 -0700

    config folder

---
+++ stop.py
@@ -0,0 +1,13 @@
+import asyncio
+import math
+import moteus
+import time
+
+async def main():
+  c = moteus.Controller(transport=None)
+   # In case the controller had faulted previously, at the start of
+    # this script we send the stop command in order to clear it.
+  await c.set_stop() # clear faults
+
+if __name__ == '__main__':
+    asyncio.run(main())
