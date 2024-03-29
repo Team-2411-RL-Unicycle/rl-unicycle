b72aa8f53b988851af9b0bff9525b8810905b1df
JacksonnF <jacksonfraser55@gmail.com>
2024-03-29 14:59:05 -0700

    added rl controller and imported onnx model

--- controller/rlcontroller.py
+++ controller/rlcontroller.py
@@ -1,12 +1,13 @@
 from controller.controllerABC import Controller, ControlInput
 #TODO add torch to the venve so that this import works (math as torch placeholder)
-import math as torch
+import onnxruntime as ort
+import numpy as np
+
 
 class RLController(Controller):
     def __init__(self, model_pth: str) -> None:
         super().__init__()
-        self.model = torch.load(model_pth)
-        self.model.eval()
+        self.model = ort.InferenceSession(model_pth)
         self.logger.info(f"{self.__class__.__name__} initialized")
 
     def get_torque(self, robot_state: ControlInput) -> float:
@@ -14,15 +15,16 @@ class RLController(Controller):
         list(robot_state)
         assert len(robot_state) == self.num_obs
 
-        #TODO: Check which device we should use
-        state = torch.zeros(size=(1, self.num_obs))
+        obs = np.zeros((1, 3))
+        obs[:, 0] = robot_state[2]
+        obs[:, 1] = robot_state[0]
+        obs[:, 2] = robot_state[1]
 
-        # Loop here for future if preprocessing necessary
-        for i, obs in enumerate(robot_state):
-            #TODO: Do tensors for trained policy need a batch size dim
-            state[:, i] = obs
+        actions = self.model.run(
+            None,
+            {"obs": obs.astype(np.float32)}
+        )[0][0]
+        
+        assert actions.shape[0] == self.num_act
 
-        action = self.model(state)
-        assert action.shape[1] == self.num_act
-
-        return action[0]
+        return actions[0]
