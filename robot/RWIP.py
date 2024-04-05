d6cd064fca1d6e8b7765bbcd1f0700d468527fca
Tristan Lee <tristan.rene.lee@gmail.com>
2024-04-04 18:04:27 -0700

    clamping torque to avoid warning spam

--- robot/RWIP.py
+++ robot/RWIP.py
@@ -106,7 +106,7 @@ class RobotSystem:
             )
 
             # Change to negative convention due to motor
-            torque_request = -self.controller.get_torque(control_input, self.MAX_TORQUE)
+            torque_request = -self.controller.get_torque(control_input, self.MAX_TORQUE - 0.001)
             self.robot_io.send_debug_data(torque_request=float(torque_request))
                         
             ## DELAY UNTIL FIXED POINT ##
