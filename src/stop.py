78d991eca81d6838c735ca1bdadaeb7dccbf9607
julianLapenna12 <7jlapenna@gmail.com>
2024-04-06 11:38:12 -0700

    set up pre-commit without black

--- src/stop.py
+++ src/stop.py
@@ -3,11 +3,13 @@ import math
 import moteus
 import time
 
+
 async def main():
-  c = moteus.Controller(transport=None)
-   # In case the controller had faulted previously, at the start of
+    c = moteus.Controller(transport=None)
+    # In case the controller had faulted previously, at the start of
     # this script we send the stop command in order to clear it.
-  await c.set_stop() # clear faults
+    await c.set_stop()  # clear faults
+
 
 if __name__ == '__main__':
     asyncio.run(main())
