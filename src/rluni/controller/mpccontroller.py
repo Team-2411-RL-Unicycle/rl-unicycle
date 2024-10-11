6ef514101b49a8ffac9c48cbb8a48f11b6395723
julianLapenna12 <7jlapenna@gmail.com>
2024-10-11 11:20:43 -0700

    start of mpc controller

---
+++ src/rluni/controller/mpccontroller.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+from rluni.controller.controllerABC import ControlInput, Controller
+from rluni.utils.utils import call_super_first
+
+
+class MPCController(Controller):
+
+    @call_super_first
+    def __init__(self) -> None:
+
+        self.logger.info(f"{self.__class__.__name__} initialized")
+
+    @call_super_first
+    def get_torque(self, robot_state: ControlInput, max_torque: float) -> float:
+        """
+        Calculates a torque using the optimal LQR gain matrix multiplied by the current robot state.
+        If the calculated torque is greater than the specified maximum, a warning message will be logged
+        and the torque will be clamped.
+
+        Returns:
+            torque: Desired torque for the MPC controller.
+        """
+        # Robot states vector
+        state_vector = np.array(
+            [
+                robot_state.pendulum_angle,
+                robot_state.pendulum_vel,
+                robot_state.wheel_vel,
+            ]
+        )
+
+        # Torque computation
+        torque = -np.dot(self._K, state_vector)
+
+        # Clamp torque if outside bounds
+        if abs(torque) > max_torque:
+            torque = max_torque * (1 if torque > 0 else -1)
+
+        return torque
