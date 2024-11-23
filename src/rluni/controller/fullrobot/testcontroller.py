8621a393d194144c7cdd4da442048b2c506eac00
Kyle Mackenzie <1mackenziekyle@gmail.com>
2024-11-22 16:43:12 -0800

    motors spin sinusoidally

--- src/rluni/controller/fullrobot/testcontroller.py
+++ src/rluni/controller/fullrobot/testcontroller.py
@@ -1,11 +1,15 @@
+import numpy as np 
+import math
 from rluni.controller.fullrobot.controllerABC import ControlInput, Controller
 from rluni.utils.utils import call_super_first
 
 
 class TestController(Controller):
 
+
     @call_super_first
     def __init__(self) -> None:
+        # self.gen = self.sine_sequence()
         self.MAX_TORQUE = 0.1
         self.logger.info(f"{self.__class__.__name__} initialized")
 
@@ -13,6 +17,13 @@ class TestController(Controller):
         """A generic test mode for the RWIP"""
         # TESTING: A simple control decision for testing
         # Match a proportional response to the detected angle
-        torques = [0.05,0.05,0.005] # , , yaw
-        return torques
-        
+        # amplitude = next(self.gen)
+        torques = amplitude * np.array([0.05,0.05,0.005]) # , , yaw
+        # torques =  np.array([0.05,0.05,0.005]) # , , yaw
+        return torques        
+    
+    def sine_sequence(self, start_x=0): 
+        i = 0 
+        while True: 
+            yield math.sin(start_x + i/10)
+            i += 1
