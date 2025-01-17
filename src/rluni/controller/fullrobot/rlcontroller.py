c7205217e8ce0525b75176584e8e4e15f823ec2f
JacksonnF <jacksonfraser55@gmail.com>
2025-01-16 16:31:17 -0800

    removed rnn

--- src/rluni/controller/fullrobot/rlcontroller.py
+++ src/rluni/controller/fullrobot/rlcontroller.py
@@ -16,15 +16,8 @@ class RLController(Controller):
         self.model = ort.InferenceSession(model_pth)
         self.logger.info(f"{self.__class__.__name__} initialized")
 
-        self.hidden_state = np.zeros((1, 1, 64)).astype(np.float32)
-        self.output_state = np.zeros((1, 1, 64)).astype(np.float32)
-        print([m.name for m in self.model.get_inputs()])
-
     @call_super_first
     def get_torques(self, robot_state: ControlInput, max_torque: float):
-        #         list(robot_state)
-        #       assert len(robot_state) == self.num_obs
-
         obs = np.zeros((1, 9))
         obs[:, 0] = robot_state.motor_speeds_roll_rads_s
         obs[:, 1] = robot_state.motor_speeds_pitch_rads_s
@@ -40,14 +33,10 @@ class RLController(Controller):
             None,
             {
                 "obs": obs.astype(np.float32),
-                "out_state.1": self.output_state,
-                "hidden_state.1": self.hidden_state,
             },
         )
 
         actions = output[0][0]
-        self.hidden_state = output[-1]
-        self.output_state = output[-2]
 
         # TODO: assert actions.shape[0] == self.num_act
         out = torques(None, None, None)
