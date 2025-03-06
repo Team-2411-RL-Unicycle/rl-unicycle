c95cee89499b49fac1d01fd4695ee0b5e00ba06c
JacksonnF <jacksonfraser55@gmail.com>
2025-03-05 16:01:09 -0800

    jackson rl yay

--- src/rluni/controller/fullrobot/rlcontroller.py
+++ src/rluni/controller/fullrobot/rlcontroller.py
@@ -17,8 +17,8 @@ class RLController(Controller):
         self.logger.info(f"{self.__class__.__name__} initialized")
         self.rnn = True
         if self.rnn:
-            self.out_state = np.zeros((1, 1, 32)).astype(np.float32)
-            self.hidden_state = np.zeros((1, 1, 32)).astype(np.float32)
+            self.out_state = np.zeros((1, 1, 64)).astype(np.float32)
+            self.hidden_state = np.zeros((1, 1, 64)).astype(np.float32)
 
     @call_super_first
     def get_torques(self, robot_state: ControlInput, max_torque: float):
@@ -26,12 +26,12 @@ class RLController(Controller):
         obs[:, 0] = robot_state.motor_speeds_roll_rads_s
         obs[:, 1] = robot_state.motor_speeds_pitch_rads_s
         obs[:, 2] = robot_state.motor_speeds_yaw_rads_s
-        obs[:, 4] = robot_state.euler_angle_pitch_rads +1.55*np.pi/180 #1.8
-        obs[:, 3] = robot_state.euler_angle_roll_rads -1.3*np.pi/180 #1.3 is best
+        obs[:, 3] = robot_state.euler_angle_roll_rads 
+        obs[:, 4] = robot_state.euler_angle_pitch_rads 
         # obs[:, 5] = robot_state.euler_angle_yaw_rads
-        obs[:, 5] = robot_state.euler_rate_yaw_rads_s
-        obs[:, 7] = robot_state.euler_rate_pitch_rads_s
-        obs[:, 6] = robot_state.euler_rate_roll_rads_s
+        obs[:, 5] = robot_state.euler_rate_roll_rads_s
+        obs[:, 6] = robot_state.euler_rate_pitch_rads_s
+        obs[:, 7] = robot_state.euler_rate_yaw_rads_s
 
         output = self.model.run(
             None,
@@ -51,10 +51,8 @@ class RLController(Controller):
         self.out_state = output[3]
         self.hidden_state = output[4]
         max_torque = 1.4
-        roll = -np.clip(
-            2.0 * actions[0], a_min=-max_torque, a_max=max_torque
-        )  # CHECK SIGNS @JACKSON
-        pitch = -np.clip(2.0 * actions[1], a_min=-max_torque, a_max=max_torque)/4
+        roll = np.clip(max_torque * roll_action_scaled, a_min=-max_torque, a_max=max_torque)/2
+        pitch = np.clip(max_torque * pitch_action_scaled, a_min=-max_torque, a_max=max_torque)/2
         # yaw = -np.clip(0.17 * actions[2], a_min=-0.17, a_max=0.17)
         yaw = 0.0
         out = torques(roll, pitch, yaw)
