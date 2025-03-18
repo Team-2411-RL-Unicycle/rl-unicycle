0114555e3ac177fd472580c2e723af60789d5558
Simon G <sghyselincks@gmail.com>
2025-03-18 11:19:58 -0700

    fix for circular import

--- src/rluni/controller/fullrobot/__init__.py
+++ src/rluni/controller/fullrobot/__init__.py
@@ -1,5 +1,4 @@
 from .controllerABC import ControlInput, Controller
-from .highlevel_xbox import HighLevelXboxController
 from .lqrcontroller import LQRController
 from .mpccontroller import MPCController
 from .pidcontroller import PIDController
@@ -7,3 +6,4 @@ from .rlcontroller import RLController
 from .testcontroller import TestController
 from .torque_filter import TorqueFilter
 from .yaw_controller import YawController
+from .highlevel_xbox import HighLevelXboxController
