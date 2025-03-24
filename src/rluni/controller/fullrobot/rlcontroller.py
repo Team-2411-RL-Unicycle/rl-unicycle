8f3b254a2277ebbbc218989ad8d208242c68c70e
JacksonnF <jacksonfraser55@gmail.com>
2025-03-24 14:48:51 -0700

    ahh

--- src/rluni/controller/fullrobot/rlcontroller.py
+++ src/rluni/controller/fullrobot/rlcontroller.py
@@ -15,32 +15,30 @@ class RLController(Controller):
     def __init__(self, model_pth: str) -> None:
         self.model = ort.InferenceSession(model_pth)
         self.logger.info(f"{self.__class__.__name__} initialized")
-        self.rnn = True
+        self.rnn = False
         if self.rnn:
             self.out_state = np.zeros((1, 1, 32)).astype(np.float32)
             self.hidden_state = np.zeros((1, 1, 32)).astype(np.float32)
 
     @call_super_first
     def get_torques(self, robot_state: ControlInput, max_torque: float):
-        obs = np.zeros((1, 8))
-        obs[:, 0] = robot_state.motor_speeds_roll_rads_s
-        obs[:, 1] = robot_state.motor_speeds_pitch_rads_s
-        obs[:, 2] = robot_state.motor_speeds_yaw_rads_s
-        obs[:, 3] = robot_state.euler_angle_roll_rads #- 4*np.pi/180
-        obs[:, 4] = robot_state.euler_angle_pitch_rads #-3*np.pi/180
+        obs = np.zeros((1, 7))
+        obs[:, 0] = robot_state.motor_speeds_roll_rads_s/2.5
+        obs[:, 1] = robot_state.motor_speeds_pitch_rads_s/2.5
+        # obs[:, 2] = robot_state.motor_speeds_yaw_rads_s
+        obs[:, 2] = robot_state.euler_angle_roll_rads - 1*np.pi/180
+        obs[:, 3] = robot_state.euler_angle_pitch_rads - 2*np.pi/180
         # obs[:, 5] = robot_state.euler_angle_yaw_rads
-        obs[:, 5] = robot_state.euler_rate_roll_rads_s
-        obs[:, 6] = robot_state.euler_rate_pitch_rads_s
-        obs[:, 7] = robot_state.euler_rate_yaw_rads_s
-
-        # print(robot_state.euler_rate_roll_rads_s)
+        obs[:, 4] = robot_state.euler_rate_roll_rads_s
+        obs[:, 5] = robot_state.euler_rate_pitch_rads_s
+        obs[:, 6] = robot_state.euler_rate_yaw_rads_s
 
         output = self.model.run(
             None,
             {
                 "obs": obs.astype(np.float32),
-                "out_state.1": self.out_state,
-                "hidden_state.1": self.hidden_state,
+                # "out_state.1": self.out_state,
+                # "hidden_state.1": self.hidden_state,
             },
         )
 
@@ -50,11 +48,11 @@ class RLController(Controller):
         roll_action_scaled = np.random.normal(actions[0], sigmas[0])
         pitch_action_scaled = np.random.normal(actions[1], sigmas[1])
 
-        self.out_state = output[3]
-        self.hidden_state = output[4]
+        # self.out_state = output[3]
+        # self.hidden_state = output[4]
         max_torque = 1.4
-        roll = np.clip(max_torque * actions[0], a_min=-max_torque, a_max=max_torque)/2
-        pitch = np.clip(max_torque * actions[1], a_min=-max_torque, a_max=max_torque)/8
+        roll = np.clip(max_torque * roll_action_scaled, a_min=-max_torque, a_max=max_torque)/2
+        pitch = np.clip(max_torque * pitch_action_scaled, a_min=-max_torque, a_max=max_torque)/4
         # yaw = -np.clip(0.17 * actions[2], a_min=-0.17, a_max=0.17)
         yaw = 0.0
         out = torques(roll, pitch, yaw)
