6cc6672d80833f662fd96dac7c8a2a89a8ab31a8
JacksonnF <jacksonfraser55@gmail.com>
2025-03-24 16:44:18 -0700

    yay rl working

--- src/rluni/controller/fullrobot/rlcontroller.py
+++ src/rluni/controller/fullrobot/rlcontroller.py
@@ -26,8 +26,8 @@ class RLController(Controller):
         obs[:, 0] = robot_state.motor_speeds_roll_rads_s/2.5
         obs[:, 1] = robot_state.motor_speeds_pitch_rads_s/2.5
         obs[:, 2] = robot_state.motor_speeds_yaw_rads_s*0
-        obs[:, 3] = robot_state.euler_angle_roll_rads + 0.0*np.pi/180
-        obs[:, 4] = robot_state.euler_angle_pitch_rads + 3.0*np.pi/180
+        obs[:, 3] = robot_state.euler_angle_roll_rads - 0.2*np.pi/180 # -0.2 is current best
+        obs[:, 4] = robot_state.euler_angle_pitch_rads + 3.3*np.pi/180 # + 3.0 current best
         # obs[:, 5] = robot_state.euler_angle_yaw_rads
         obs[:, 6] = robot_state.euler_rate_roll_rads_s
         obs[:, 7] = robot_state.euler_rate_pitch_rads_s
