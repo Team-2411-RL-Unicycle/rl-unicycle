2458b284c5dfd97e3058ec7313a8189a94eb68af
JacksonnF <jacksonfraser55@gmail.com>
2025-03-24 16:54:35 -0700

    removed env

--- venv_rluni/lib/python3.12/site-packages/cvxpy/reductions/dgp2dcp/canonicalizers/finite_set_canon.py
+++
@@ -1,7 +0,0 @@
-from cvxpy.constraints.finite_set import FiniteSet
-
-
-def finite_set_canon(expr, args):
-    ineq_form, id = expr.get_data()
-    # Log applied already to set of values (i.e., args[1]).
-    return FiniteSet(args[0], args[1], ineq_form, id), []
--- venv_rluni/lib/python3.12/site-packages/cvxpy/utilities/deterministic.py
+++
@@ -1,8 +0,0 @@
-def unique_list(duplicates_list):
-    """
-    Return unique list preserving the order.
-    https://stackoverflow.com/a/480227
-    """
-    used = set()
-    unique = [x for x in duplicates_list if not (x in used or used.add(x))]
-    return unique
